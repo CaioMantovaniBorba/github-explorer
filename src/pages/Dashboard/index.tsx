170802fe296d2e7573abc82c482906b73cc91d32
Caio Mantovani Borba <caiovmborba@gmail.com>
2020-07-12 14:21:30 -0300

    Stylize Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,61 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no GitHub</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="GitHub Explorer" />
+      <Title>Explore repositórios no GitHub</Title>
+
+      <Form>
+        <input type="text" placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="/">
+          <img
+            src="https://api.adorable.io/avatars/100/abott@adorable.png"
+            alt="Avatar"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms! </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="/">
+          <img
+            src="https://api.adorable.io/avatars/100/abott@adorable.png"
+            alt="Avatar"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms! </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="/">
+          <img
+            src="https://api.adorable.io/avatars/100/abott@adorable.png"
+            alt="Avatar"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms! </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
